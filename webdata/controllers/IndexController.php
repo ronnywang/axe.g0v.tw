ffb6179c5b1fd3013e533702046e3991ef6935df
Ronny Wang <ronnywang@gmail.com>
2014-02-16 22:10:49 +0800

    加上檢查答案並記住挑戰成功名單功能

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -15,4 +15,36 @@ class IndexController extends Pix_Controller
             return $this->redirect('/');
         }
     }
+
+    public function checkAction()
+    {
+        list(, /*index*/, /*check*/, $id) = explode('/', $this->getURI());
+        $level = Level::find_by_level_no($id);
+
+        if (!$level) {
+            return $this->alert("找不到關卡 {$level}", '/');
+        }
+
+        $answer = $_POST['answer'];
+        if (!$answer = json_decode($answer)) {
+            return $this->alert("不是合法的 JSON", '/level/' . $id);
+        }
+
+        $ret = $level->check($answer);
+        if (!$ret) {
+            return $this->alert("答案不正確", "/level/{$id}");
+        }
+
+        if ($_POST['name']) {
+            LevelHero::insert(array(
+                'level_id' => $level->id,
+                'name' => mb_strimwidth(strval($_POST['name']), 0, 16),
+                'created_at' => time(),
+                'created_from' => intval(ip2long($_SERVER['REMOTE_ADDR'])),
+            ));
+        }
+
+        return $this->alert("挑戰成功!在下面留言把你的程式碼傳到 GitHub 分享給大家吧!", "/level/{$id}");
+
+    }
 }
